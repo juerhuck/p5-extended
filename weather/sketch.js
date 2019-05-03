2ce9e6a6fcd41717cee0df0465e31bb76c8389c4
Juergen Hucke <juergen.hucke@ima.de>
2019-05-03 08:03:27 +0200

    weather basic.

--- weather/sketch.js
+++ weather/sketch.js
@@ -1,23 +1,24 @@
 var weather;
 
 //https://openweathermap.org/current
-
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=001b0f58045147663b1ea518d34d88b4&units=metric';
 
 function setup() {
   createCanvas(400, 200);
-  loadJSON('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=001b0f58045147663b1ea518d34d88b4&units=metric', gotData);
+  loadJSON(url, gotData);
 }
 
 
 function gotData(data) {
-  // console.log(data);
   weather = data;
 }
 
 function draw() {
   background(0);
   if (weather) {
-    ellipse(100, 100, weather.main.temp, weather.main.temp);
-    ellipse(300, 100, weather.main.humidity, weather.main.humedity);
+    var temp = weather.main.temp;
+    var humidity = weather.main.humidity;
+    ellipse(100, 100, temp, temp);
+    ellipse(300, 100, humidity, humidity);
   }
 }
